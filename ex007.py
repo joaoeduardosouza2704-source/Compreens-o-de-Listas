92294c1ff4fe67148524131af164bb9dc16388b2
João Edaurdo <joaoeduardosouza2704@gmail.com>
2026-06-26 13:34:00 -0300

    7

--- ex007.py
+++
@@ -1,5 +0,0 @@
-#Tamanha de cada palavra
-def tamanho_cada_palavra(lista_palavras):
-    return [len(palavra) for palavra in lista_palavras]
-lista_palavras = list(input().split())
-print(tamanho_cada_palavra(lista_palavras))
